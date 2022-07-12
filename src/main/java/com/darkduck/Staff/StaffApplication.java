3a6b3b9c3653d5368aeb0c6b3f05ecf02e9d7b87
darkduckdd <mr.adilbek75@mail.ru>
2022-07-12 14:33:22 +0600

    time

--- src/main/java/com/darkduck/Staff/StaffApplication.java
+++ src/main/java/com/darkduck/Staff/StaffApplication.java
@@ -7,12 +7,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.GregorianCalendar;
+
 @SpringBootApplication
+@EnableScheduling
 public class StaffApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StaffApplication.class, args);
+    }
+
+    @Scheduled(fixedRate = 50000)
+    public void printDate() {
+        System.out.println("RC Time Service: " + new GregorianCalendar().getTime());
     }
 }
